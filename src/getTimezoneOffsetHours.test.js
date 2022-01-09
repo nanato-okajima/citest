abef131a362570dbc0cf4aa4de15707eccbf6f61
naoto-naka <luckywhitecat770@gmail.com>
2022-01-09 14:19:28 +0900

    Fix fail test

--- src/getTimezoneOffsetHours.test.js
+++ src/getTimezoneOffsetHours.test.js
@@ -1,7 +1,5 @@
-const { expect } = require('@jest/globals')
-const { test } = require('picomatch')
 const getTimeZoneOffsetHours = require('./getTimezoneOffsetHours')
 
 test('getTimezoneOffsetHours return -9', () => {
-    expect(getTimeZoneOffsetHours()).toEqual(-9)
+    expect(getTimeZoneOffsetHours()).toBe(-9)
 })
